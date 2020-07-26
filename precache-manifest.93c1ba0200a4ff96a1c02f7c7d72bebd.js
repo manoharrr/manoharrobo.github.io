7670ad602224fac99472ab5bf97e57302cfd79b3
Manohar <manoharmanu95@gmail.com>
2020-07-26 15:00:50 +0530

    Updates

---
+++ precache-manifest.93c1ba0200a4ff96a1c02f7c7d72bebd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bec730082d3480a0aaed03dd4f91da8",
+    "url": "/manoharrobo.github.io/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/manoharrobo.github.io/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "42eea51bdf254d730698",
+    "url": "/manoharrobo.github.io/static/css/main.62d61e75.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/manoharrobo.github.io/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manoharrobo.github.io/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42eea51bdf254d730698",
+    "url": "/manoharrobo.github.io/static/js/main.ff5fe7cf.chunk.js"
+  },
+  {
+    "revision": "d8486557e8fe4a4d07eb",
+    "url": "/manoharrobo.github.io/static/js/runtime-main.efdb706a.js"
+  }
+]);
